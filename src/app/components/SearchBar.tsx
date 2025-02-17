8b6c1d2319760560b0dea4821bcdda0d45537c31
arman-31 <armanboncale31@gmail.com>
2025-02-17 20:37:52 +0800

    commit

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -9,7 +9,7 @@ export default function SearchBar() {
             className="w-full px-4 py-2 border voder-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
             <button className="absolute right-2 top-2 border-gray-300 rounded-md focus:outline-none foucs:ring-2 focus:ring-blue-500">
-             <Search className="h-10 w-10 text-gray-400" />
+             <Search className="h-8 w-8 text-gray-400" />
             </button>
         </div>
     )
