27ce27735e165bccf0cf15af3273383fd9e0d7c3
arman-31 <armanboncale31@gmail.com>
2025-02-17 20:42:23 +0800

    commit

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -6,9 +6,9 @@ export default function SearchBar() {
             <input
             type="text"
             placeholder="Search..."
-            className="w-full px-4 py-2 border voder-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
-            <button className="absolute right-1 top-3 border-gray-300 rounded-md focus:outline-none foucs:ring-2 focus:ring-blue-500">
+            <button className="absolute right-2 top-1/2 transform -translate-y-1/2">
              <Search className="h-8 w-8 text-gray-400" />
             </button>
         </div>
