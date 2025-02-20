a91eaf8c7e9f7c54e80222a010f5fbbea4c9e6fc
arman-31 <armanboncale31@gmail.com>
2025-02-20 19:30:31 +0800

    commit

--- src/app/order-conformation/page.tsx
+++ src/app/order-conformation/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from "next/navigation"
 import prisma from "../lib/prisma"
-import Header from "../components/Header"
+import { Header } from "../components/Header"
 import { Button } from "app/components/ui/button"
 
 export default async function OrderConfirmationPage({ params }: { params: { id: string } }) {
