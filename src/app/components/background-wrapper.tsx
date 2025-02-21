cec635fe6711d9ba63a2a118cf2bbbeafc199aae
arman-31 <armanboncale31@gmail.com>
2025-02-21 16:34:42 +0800

    commit

--- src/app/components/background-wrapper.tsx
+++ src/app/components/background-wrapper.tsx
@@ -6,7 +6,7 @@ export function BackgroundWrapper({ children }: { children: React.ReactNode }) {
     <div className="relative min-h-screen">
       {/* Fixed background image */}
       <div className="fixed inset-0 -z-10">
-        <Image src="/placeholder.svg?height=1080&width=1920" alt="Background" fill className="object-cover" priority />
+        <Image src="/background.png?height=1080&width=1920" alt="Background" fill className="object-cover" priority />
         {/* Subtle gradient overlay */}
         <div className="absolute inset-0 bg-gradient-to-b from-white/10 to-white/30 dark:from-gray-950/10 dark:to-gray-950/30" />
       </div>
