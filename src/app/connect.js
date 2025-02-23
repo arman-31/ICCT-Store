55e405c6249f1e421723c5eb616b32ccd9edfbea
arman-31 <armanboncale31@gmail.com>
2025-02-23 15:20:03 +0800

    commit

--- src/app/connect.js
+++ src/app/connect.js
@@ -1,26 +1,13 @@
+import mongoose from "mongoose";
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Arman-31:12311561@icct-store.yoct9.mongodb.net/ICCT-Store/?retryWrites=true&w=majority&appName=ICCT-Store";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
+async function connectDB() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    await mongoose.connect("mongodb+srv://Arman-31:12311561@icct-store.yoct9.mongodb.net/ICCT-Store?retryWrites=true&w=majority");
+    console.log("✅ MongoDB Connected!");
+  } catch (err) {
+    console.error("❌ MongoDB Connection Error:", err);
+    process.exit(1);
   }
 }
-run().catch(console.dir);
+
+connectDB();
